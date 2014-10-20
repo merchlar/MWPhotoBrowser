1b8690ec4328d2ad0e5139b67d647f6752899cbf
adolfo <adolfo@Adolfos-MacBook-Pro-2.local>
2014-10-20 15:24:33 -0400

    fixed compiler issues

--- MWPhotoBrowser/Libraries/PSTCollectionView/PSTCollectionViewLayout.m
+++ MWPhotoBrowser/Libraries/PSTCollectionView/PSTCollectionViewLayout.m
@@ -9,6 +9,8 @@
 #import "PSTCollectionViewItemKey.h"
 #import "PSTCollectionViewData.h"
 
+#import <objc/runtime.h>
+
 @interface PSTCollectionView ()
 - (id)currentUpdate;
 - (NSDictionary *)visibleViewsDict;
@@ -161,7 +163,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - PSTCollection/UICollection interoperability
 
-#import <objc/runtime.h>
+
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)selector {
     NSMethodSignature *signature = [super methodSignatureForSelector:selector];
     if (!signature) {
